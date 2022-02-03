c710beae0b7a960249ca959d59011a3248278880
harishkumar6381 <emvharish20@gmail.com>
2022-02-03 17:42:48 +0530

    floder structure

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class Employee {
+	
+	public void empId() {
+		
+		System.out.println("employee id is 1234");
+
+	}
+    public void empName() {
+        System.out.println("employee name is bala");	
+}
+}
